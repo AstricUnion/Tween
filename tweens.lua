4a760cdb7d41583a6b4803c1ea2bd1c6c732b9ef
maxobur0001 <msblock09@gmail.com>
2026-08-24 22:11:45 +0300

    FCurve function changed to work with different curve intervals

--- tweens.lua
+++ tweens.lua
@@ -152,10 +152,24 @@ end
 
 local function longBezier(process, keyframes)
     local len = #keyframes
-    if len < 2 then return keyframes[1] and keyframes[1][1][2] or 0 end
-    local globalProcess = math.min(process * len, len - 1)
-    local currentCurve = process == 0 and 1 or math.ceil(globalProcess)
+    if len < 2 then return keyframes[1] and keyframes[1][2][2] or 0 end
+    local startFrame = keyframes[1][2][1]
+    local endFrame = keyframes[len][2][1]
+    local frameLen = endFrame - startFrame
 
+    local offset = (process * frameLen) + startFrame
+    local currentCurve = len - 1
+    for i, keyframe in ipairs(keyframes) do
+        if i == 1 then goto cont end
+        local localEndFrame = keyframe[2][1]
+        local position = localEndFrame - startFrame
+        local positionDivided = position / frameLen
+        if process < positionDivided then
+            currentCurve = i - 1
+            break
+        end
+        ::cont::
+    end
     local curve = keyframes[currentCurve]
     local nextCurve = keyframes[currentCurve + 1]
     local start = Vector(curve[2][1], curve[2][2])
@@ -163,10 +177,30 @@ local function longBezier(process, keyframes)
     local tangent2 = Vector(nextCurve[1][1], nextCurve[1][2])
     local _end = Vector(nextCurve[2][1], nextCurve[2][2])
 
-    local value = math.bezierVectorCubic(globalProcess - currentCurve + 1, start, tangent1, tangent2, _end)
+    local start1 = start[1]
+    local value = math.bezierVectorCubic((offset - start1) / (_end[1] - start1), start, tangent1, tangent2, _end)
 
     return value.y
 end
+
+-- Old version of this
+-- local function longBezier(process, keyframes)
+--     local len = #keyframes
+--     if len < 2 then return keyframes[1] and keyframes[1][1][2] or 0 end
+--     local globalProcess = math.min(process * len, len - 1)
+--     local currentCurve = process == 0 and 1 or math.ceil(globalProcess)
+--
+--     local curve = keyframes[currentCurve]
+--     local nextCurve = keyframes[currentCurve + 1]
+--     local start = Vector(curve[2][1], curve[2][2])
+--     local tangent1 = Vector(curve[3][1], curve[3][2])
+--     local tangent2 = Vector(nextCurve[1][1], nextCurve[1][2])
+--     local _end = Vector(nextCurve[2][1], nextCurve[2][2])
+--
+--     local value = math.bezierVectorCubic(globalProcess - currentCurve + 1, start, tangent1, tangent2, _end)
+--
+--     return value.y
+-- end
 
 local function blenderRotation(p, y, r)
     local ang = Angle()
